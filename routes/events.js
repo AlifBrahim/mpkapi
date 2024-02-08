c8b1d72b9efc5a88e80420715364cb2f809943a5
zulfanjava <2021828582@student.uitm.edu.my>
2024-02-08 11:44:43 +0800

    Fix Import

--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,18 @@
-import pool from "express";
-import router from "./index";
+var express = require('express');
+var router = express.Router();
+
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const pool = mysql.createPool({
+  host: 'uumevents-do-user-14295301-0.b.db.ondigitalocean.com',
+  port: '25060',
+  user: 'doadmin',
+  password: 'AVNS_7KPMC3xu3yCp_jz_WfT',
+  database: 'mpk'
+});
+
+router.use(bodyParser.json());
 
 router.post('/signup', async (req, res) => {
   const { userID, password } = req.body;
